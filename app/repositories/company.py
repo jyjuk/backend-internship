26c4c90221095978bc06bea09837513f9437cb82
Ivan Havrylyshyn <ivan.havrylyshyn@meduzzen.com>
2025-11-24 12:41:43 +0200

    Create BaseRepository and refactor UserRepository and CompanyRepository to inherit from it

--- app/repositories/company.py
+++ app/repositories/company.py
@@ -3,27 +3,14 @@ from uuid import UUID
 from sqlalchemy import select, func
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.models.company import Company
+from app.repositories.base import BaseRepository
 
 
-class CompanyRepository:
+class CompanyRepository(BaseRepository[Company]):
     """Repository for Company model"""
 
     def __init__(self, session: AsyncSession):
-        self.session = session
-
-    async def create(self, company: Company) -> Company:
-        """Create new company"""
-        self.session.add(company)
-        await self.session.commit()
-        await self.session.refresh(company)
-        return company
-
-    async def get_by_id(self, company_id: UUID) -> Optional[Company]:
-        """Get company by ID"""
-        result = await self.session.execute(
-            select(Company).where(Company.id == company_id)
-        )
-        return result.scalar_one_or_none()
+        super().__init__(Company, session)
 
     async def get_all(self, skip: int = 0, limit: int = 100, visible_only: bool = True) -> List[Company]:
         """Get all companies with pagination"""
@@ -41,17 +28,6 @@ class CompanyRepository:
             query = query.where(Company.is_visible == True)
         result = await self.session.execute(query)
         return result.scalar_one()
-
-    async def update(self, company: Company) -> Company:
-        """Update company"""
-        await self.session.commit()
-        await self.session.refresh(company)
-        return company
-
-    async def delete(self, company: Company) -> None:
-        """Delete company"""
-        await self.session.delete(company)
-        await self.session.commit()
 
     async def get_by_owner(self, owner_id: UUID, skip: int = 0, limit: int = 100) -> List[Company]:
         """Get all companies owned by user"""
